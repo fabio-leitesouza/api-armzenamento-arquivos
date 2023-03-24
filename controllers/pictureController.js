b64dab34b66a5771960fe7a359f559b456cd07da
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2023-03-24 16:47:37 -0300

    add delete

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -1,5 +1,7 @@
 const Picture = require("../models/Picture");
 
+const fs = require("fs")
+
 exports.create = async (req, res) => {
     try {
         const { name } = req.body;
@@ -25,8 +27,28 @@ exports.findAll = async (req, res) => {
         const pictures = await Picture.find()
 
         res.json(pictures);
-        
+
     } catch (error) {
-        res.status(500).json({message: "Esso ao buscar imagem"})
+        res.status(500).json({message: "Erro ao buscar imagem"})
     }
+}
+
+exports.remove = async(req, res) => {
+        try {
+
+            const picture = await Picture.findById(req.params.id)
+
+            if(!picture) {
+                return res.status(404).json({message: "Imagem não encontrada"})
+            }
+
+            fs.unlinkSync(picture.src)
+
+            await picture.remove()
+
+            res.json({message: "Imagem removida com sucesso"})
+            
+        } catch (error) {
+            res.status(500).json({message: "Erro ao excluir imagem"})
+        }
 }
